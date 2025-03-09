ae5b5a30cbaaa80ccd71a9499a756e8ff50ff710
cristianosts <cristianosantosreal@gmail.com>
2025-03-09 20:46:38 -0300

    first config

---
+++ patisserie-lpage/src/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <h1 className="">Cristiano de Jesus Santos eu estou aqui á pelo menos uns tres anos e meio</h1>
+  );
+}
