2ccd91a473fc54e3cc0a89d049901232017ae7a5
cristianosts <cristianosantosreal@gmail.com>
2025-05-08 23:56:00 -0300

    Update sectionIntro.tsx

--- patisserie-lpage/src/components/sectionIntro.tsx
+++ patisserie-lpage/src/components/sectionIntro.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image"
+import Image from "next/image" 
 import LogoType from "./assets/logoname.png"
 import QueroLogo from "./assets/quero.png"
 import IfoodLogo from "./assets/ifoodlogo.png"
